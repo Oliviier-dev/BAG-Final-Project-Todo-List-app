c9db31f30f9ea394bf4686f87275dcde37ec2ed1
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-11-24 18:32:33 +0200

    Added the function addTodo which adds a new todo item

--- app.js
+++ app.js
@@ -30,3 +30,47 @@ title.textContent = `${userName.charAt(0).toUpperCase() + userName.slice(1).toLo
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", updateTodo);
+
+
+//function addTodo which adds a new todo
+function addTodo(e){
+
+    e.preventDefault();
+
+    //create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    
+    //create list
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+
+    //Save to local
+    saveLocalTodos(todoInput.value);
+
+    //creating new todo item
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    todoInput.value = "";
+
+    //star button
+    const starButton = document.createElement("button");
+    starButton.innerHTML = `<i class="fas fa-star"></i>`;
+    starButton.classList.add("star-btn");
+    todoDiv.insertBefore(starButton, todoDiv.children[0]);
+
+    //completed button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //insert the whole div before other todos
+    todoList.insertBefore(todoDiv, todoList.children[0]);
+}
