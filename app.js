4f65cd97f7b1140dcf6b79dbe2989388f21c7145
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-11-24 18:33:54 +0200

    Added the updateTodo function which stars, completes, and deletes todo

--- app.js
+++ app.js
@@ -74,3 +74,80 @@ function addTodo(e){
     //insert the whole div before other todos
     todoList.insertBefore(todoDiv, todoList.children[0]);
 }
+
+
+//function which deletes, star, and mark todo items as completed
+
+function updateTodo(e){
+    const item = e.target;
+
+    //if the user deletes the item
+    if(item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        removeLocalTodos(todo);
+        todo.addEventListener("transitionend", e => {
+            todo.remove();
+        });
+    }
+
+    //if the user marks the todo item as completed
+    if(item.classList[0] === "complete-btn"){
+
+        const todo = item.parentElement;
+         if (todo.classList.contains("completed")) {
+            if(todo.classList.contains("important")){
+                todo.classList.remove("completed");
+                todoList.insertBefore(todo, todoList.children[0]);
+            }
+            countCompletedTodo -= 1;
+            
+            // If the todo item is marked as completed, and there are multiple completed items
+            if (document.querySelectorAll('.todo.completed').length > 1) {
+                // Move the completed item to the top of completed items
+                const completedItems = [...document.querySelectorAll('.todo.completed')];
+                const firstCompletedItem = completedItems[0];
+                todo.classList.remove("completed");
+                todoList.insertBefore(todo, firstCompletedItem);
+            } else{
+                todo.classList.remove("completed");
+            }
+        } else {
+            countCompletedTodo += 1;
+            // If the todo item is not marked as completed, mark it as completed and move it to the bottom
+            todo.classList.add("completed");
+            todoList.appendChild(todo);
+        }
+    }
+
+    //If the user stars the todo
+    if(item.classList[0] === "star-btn"){
+        
+        const todo = item.parentElement;
+        const starButton = item.parentElement.children[0];
+        const star = item.querySelector(".fa-star");
+        
+        //if the user clicks the star when the item is starred; the star is removed
+        if(todo.classList.contains("important")){
+            //making sure that the completed items aren't allowed to access this feature
+            if(!todo.classList.contains("completed")){
+                star.style.color = "black";
+                todo.style.background = "white";
+                starButton.style.background = "white";
+                todo.classList.remove("important");
+                todoList.insertBefore(todo,todoList.children[[...document.querySelectorAll('.todo')].length - countCompletedTodo]);
+            }
+
+        } else{
+            //making sure that the completed items aren't allowed to access this feature
+            if(!todo.classList.contains("completed")){
+                todo.classList.add("important");
+                star.style.color = "#47e6ff";
+                starButton.style.background = "#0bd484";
+                todo.style.background = "#0bd484";
+                
+                todoList.insertBefore(todo, todoList.children[0]);
+            }
+        }
+    }
+}
