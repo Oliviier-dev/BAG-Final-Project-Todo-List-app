9950bf3273186cfb095f27c574b1a1142a014da3
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-11-24 18:29:09 +0200

    Grabbed important elements with DOM

---
+++ app.js
@@ -0,0 +1,6 @@
+//selecting the elements
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const starButton = document.querySelector(".star-btn");
+const todoList = document.querySelector(".todo-list");
+const title = document.getElementById("title");
